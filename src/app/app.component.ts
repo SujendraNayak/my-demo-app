b648fb0a57088838d4f6091ec8d0c2bb5e38f2e6
SujendraNayak <sujinayak008@gmail.com>
2024-12-05 23:18:04 +0530

    calculater update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-demo-app';
-}
+  result:string="";
+
+  add(firstNumber:string,secondNumber:string){
+    this.result=(Number(firstNumber)+Number(secondNumber)).toString();
+  }
+  subtract(firstNumber:string,secondNumber:string){
+    this.result=(Number(firstNumber)-Number(secondNumber)).toString();
+  }
+  multiply(firstNumber:string,secondNumber:string){
+    this.result=(Number(firstNumber)*Number(secondNumber)).toString();
+  }
+  divide(firstNumber:string,secondNumber:string){
+    this.result=(Number(firstNumber)/Number(secondNumber)).toString();
+  }
+};
+
